9e1a64d6995100e425a4513bd2eca79e845f047c
costa <kiroloscosta@gmail.com>
2019-04-15 10:22:12 +0200

    Add project files.

---
+++ NurseryManager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NurseryManager.Startup))]
+namespace NurseryManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
